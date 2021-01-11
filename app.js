01e1af1341c94e1f4aa268509f3eafae84a0caa9
JeremyRivera <jeremyrivera23@yahoo.com>
2021-01-11 00:47:45 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+let myName = 'Jeremy';
+
+switch(myName) {
+  case 'Raul':
+  console.log(`My name is ${myName}`)
+  break;
+  case 'ben':
+  console.log(`My name is ${myName}`)
+  break;
+  case 'Jeremy':
+  console.log(`My name is ${myName}`)
+  break;
+  default:
+  console.log('no name is here');
+  break;
+}
+//Made entire switch statment solo dolo!
